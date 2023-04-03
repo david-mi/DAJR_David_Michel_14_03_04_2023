1f3892905c035faa844f9c93e08e2e61eb58663c
David Michel <david.michel2809@gmail.com>
2023-04-03 13:48:01 +0200

    feat: add submit logic on createEmployee form, make all inputs required #5

--- src/components/CreateEmployee/CreateEmployee.tsx
+++ src/components/CreateEmployee/CreateEmployee.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react"
 import SelectMenu from "./Select/Select"
 import styles from "./createEmployee.module.css"
 import type { SelectData } from "../../pages/Home/data/types"
@@ -8,36 +9,63 @@ interface Props {
 }
 
 const CreateEmployee = ({ states, departments }: Props) => {
+  const formRef = useRef<HTMLFormElement>(null)
+
+  /**
+   * Form submit
+   */
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault()
+
+    const employeesStorage = localStorage.getItem('employees')
+    const employees = employeesStorage
+      ? JSON.parse(employeesStorage)
+      : []
+
+    const form = formRef.current!
+    const formData = new FormData(form)
+    const formBody = Object.fromEntries(formData)
+
+    employees.push(formBody)
+    localStorage.setItem("employees", JSON.stringify(employees))
+  }
+
   return (
-    <form id="create-employee" className={styles.form}>
+    <form
+      onSubmit={handleSubmit}
+      id="create-employee"
+      className={styles.form}
+      ref={formRef}
+    >
       <h2 className={styles.title}>Create Employee</h2>
 
-      <label htmlFor="first-name">First Name</label>
-      <input type="text" id="first-name" />
+      <label htmlFor="firstName">First Name</label>
+      <input type="text" id="firstName" name="firstName" required />
 
-      <label htmlFor="last-name">Last Name</label>
-      <input type="text" id="last-name" />
+      <label htmlFor="lastName">Last Name</label>
+      <input type="text" id="lastName" name="lastName" />
 
-      <label htmlFor="date-of-birth">Date of Birth</label>
-      <input id="date-of-birth" type="text" />
+      <label htmlFor="dateOfBirth">Date of Birth</label>
+      <input type="date" id="dateOfBirth" name="dateOfBirth" required />
 
-      <label htmlFor="start-date">Start Date</label>
-      <input id="start-date" type="text" />
+      <label htmlFor="startDate">Start Date</label>
+      <input type="date" id="startDate" name="startDate" required />
 
       <fieldset className={styles.address}>
         <legend>Address</legend>
 
         <label htmlFor="street">Street</label>
-        <input id="street" type="text" />
+        <input id="street" name="street" type="text" required />
 
         <label htmlFor="city">City</label>
-        <input id="city" type="text" />
+        <input id="city" name="city" type="text" required />
 
         <label htmlFor="state">State</label>
         <SelectMenu id="state" name="state" data={states} />
 
-        <label htmlFor="zip-code">Zip Code</label>
-        <input id="zip-code" type="number" />
+        <label htmlFor="zipCode">Zip Code</label>
+        <input id="zipCode" name="zipCode" type="number" required />
       </fieldset>
 
       <label htmlFor="department">Department</label>
--- src/components/CreateEmployee/Select/Select.tsx
+++ src/components/CreateEmployee/Select/Select.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const Select = ({ name, id, data }: Props) => {
   return (
-    <select name={name} id={id}>
+    <select name={name} id={id} required>
       {data.map(({ name, value }) => {
         return <option key={name} value={value || name}>{name}</option>
       })}
