be624f5f3478f3fe8afb2fe4c626eee69997b62d
David Michel <david.michel2809@gmail.com>
2023-04-04 18:20:57 +0200

    init: prepare employee page #16

--- src/pages/Employees/Employees.tsx
+++ src/pages/Employees/Employees.tsx
@@ -1,12 +1,17 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react"
+import { EmployeesContext } from "../../context/EmployeesContext"
 import Title from "../../components/Title/Title"
 
 const Employees = () => {
+  const { employees, setEmployees } = useContext(EmployeesContext)
+
   return (
     <main id="employee-div" className="container">
       <Title title="Current Employees" />
       <table id="employee-table" className="display"></table>
-      <Link to="/">Home</Link>
+      <div style={{ fontSize: "2rem" }}>
+        {JSON.stringify(employees, null, 2)}
+      </div>
     </main>
   )
 }
