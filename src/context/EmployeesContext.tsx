0e9a67c6b89cc156233ab2d9740ddd25f144557e
David Michel <david.michel2809@gmail.com>
2023-04-26 17:24:06 +0200

    feat: install latest table package and add color to disabled table buttons

--- src/context/EmployeesContext.tsx
+++ src/context/EmployeesContext.tsx
@@ -19,7 +19,7 @@ interface Employees {
   setEmployees: Dispatch<SetStateAction<Employee[]>>
 }
 
-const generatedEmployees = generateEmployees<Employee>({ amount: 1000 })
+const generatedEmployees = generateEmployees<Employee>({ amount: 10_000 })
 
 export const EmployeesContext = createContext<Employees>({
   employees: [],
