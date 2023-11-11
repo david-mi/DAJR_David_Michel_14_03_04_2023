3b98421de3bcab378f0a2796f791527ece88a39e
David Michel <david.michel2809@gmail.com>
2023-11-11 10:07:47 +0100

    feat/config: update table lib and replace table style by lib style

--- src/pages/Employees/Employees.tsx
+++ src/pages/Employees/Employees.tsx
@@ -3,8 +3,8 @@ import { EmployeesContext } from "../../context/EmployeesContext"
 import type { Employee } from "../../context/EmployeesContext"
 import Title from "../../components/Title/Title"
 import { Table } from "david-mi-react-table"
+import "david-mi-react-table/style.css"
 import { columns } from "../../data/"
-import styles from "./employees.module.css"
 
 const Employees = () => {
   const { employees, setEmployees } = useContext(EmployeesContext)
@@ -15,15 +15,6 @@ const Employees = () => {
       <Table<keyof Employee>
         columns={columns}
         rows={employees}
-        classNames={{
-          container: styles.container,
-          informations: styles.informations,
-          navigation: styles.navigation,
-          search: styles.search,
-          select: styles.select,
-          tableContainer: styles.tableContainer,
-          table: styles.table
-        }}
       />
     </main>
   )
